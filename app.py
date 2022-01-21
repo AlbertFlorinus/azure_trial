dda6c20a23b109d43358d492eddfa12b756f10f9
Albert Florinus <abbeflorinus@gmail.com>
2022-01-21 18:21:24 +0100

    _

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from flask import Flask
 from markupsafe import escape
 
+import core
+
+url = "https://lager.emilfolino.se/v2/products/everything"
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "Hello World!"
 
-@app.route("/another")
-def another():
-    return "Another Hello World!"
-
 @app.route("/dynamic/<name>")
 def dynamic(name):
     return f"Hello {escape(name)}"
+
+@app.route("/unique")
+def give_unique():
+    url = "https://lager.emilfolino.se/v2/products/everything"
+    data = core.retrieve(url)["data"]
+    df, _ = core.main(data)
+    return core.unique(df)
